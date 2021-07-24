3ebbe76a33c791f8ff191ca9a16f4351c83dfa84
Felix Cuello <felix.cuello@gmail.com>
2021-07-24 09:03:31 -0300

    [FC/LeetCode/443] Solved

---
+++ code/fc_lc_443.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int pos(vector<vector<int>>& g, int r, int c, int rs, int cs) {
+    if(r < 0 || c < 0 || r >= rs || c >= cs) return 0;
+    return g[r][c];
+  }
+
+  int islandPerimeter(vector<vector<int>>& grid) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    int result = 0;
+    for(int row=0; row<rows; row++)
+      for(int col=0; col<cols; col++) {
+        if(grid[row][col] == 0) continue;
+        
+        result += 4;
+        result -= pos(grid, row-1, col, rows, cols);
+        result -= pos(grid, row+1, col, rows, cols);
+        result -= pos(grid, row, col-1, rows, cols);
+        result -= pos(grid, row, col+1, rows, cols);
+      }
+
+    return result;
+  }
+};
