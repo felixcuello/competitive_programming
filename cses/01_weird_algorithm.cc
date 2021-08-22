4e01a10d70930b8d7f3c49153ad29c451ba5033e
Felix Cuello <felix.cuello@gmail.com>
2021-08-22 00:16:27 -0300

    Added CSES directory

--- cses/01_weird_algorithm.cc
+++ cses/01_weird_algorithm.cc
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n; cin >> n;
 
     unsigned long long x = n;
