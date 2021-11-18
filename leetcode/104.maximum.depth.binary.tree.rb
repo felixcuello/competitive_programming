d84a5a4a1230af15e98855a9809dd48e4e3a1e69
Felix Cuello <felix.cuello@gmail.com>
2021-11-17 22:34:53 -0300

    [leetcode] 104.maximum.depth.binary.tree

---
+++ leetcode/104.maximum.depth.binary.tree.rb
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+  return 0 if root.nil?
+
+  [max_depth(root.left) + 1, max_depth(root.right) + 1].max
+end
