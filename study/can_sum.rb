539c67e668eeb8a4b7581d6093c0d9234c825016
Felix Cuello <felix.cuello@gmail.com>
2022-02-12 20:23:56 -0300

    Beautified the code a bit

--- study/can_sum.rb
+++ study/can_sum.rb
@@ -1,10 +1,10 @@
+# rubocop:disable Metrics/MethodLength
 def can_sum(target_sum, numbers, memo = {})
   return memo[target_sum] if memo.key?(target_sum)
   return true if target_sum == 0
   return nil if target_sum < 0
 
   numbers.each do |n|
-
     if can_sum(target_sum - n, numbers, memo)
       memo[target_sum - n] = true
       return true
@@ -19,7 +19,7 @@ describe 'examples' do
   it 'can actually sum' do
     expect(can_sum(30, [5])).to be true
     expect(can_sum(121, [11])).to be true
-    expect(can_sum(200, [3,6,10])).to be true
+    expect(can_sum(200, [3, 6, 10])).to be true
   end
 
   it 'cannot sum' do
@@ -27,3 +27,4 @@ describe 'examples' do
     expect(can_sum(300, [7, 14])).to be false
   end
 end
+# rubocop:enable Metrics/MethodLength
