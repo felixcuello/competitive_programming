3336994b142db5fbb045ccf35c59b3d7a7ac8396
Felix <cuellof@gmail.com>
2022-03-15 22:21:01 -0300

    Update 448.find.all.numbers.disappeared.in.array.rb

--- leetcode/problems/448.find.all.numbers.disappeared.in.array.rb
+++ leetcode/problems/448.find.all.numbers.disappeared.in.array.rb
@@ -1,15 +1,34 @@
+#-------------------------------------------------------
+#  NEW: Optimal solution with no extra space
+#-------------------------------------------------------
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_disappeared_numbers(nums)
-  max = 0
-  s = Set.new
-  nums.each do |n|
-    s.add(n)
+  nums.count.times do |i|
+    pos = nums[i].abs - 1
+    nums[pos] *= -1 if nums[pos] > 0
   end
-
-  result = []
-  1.upto(nums.count) do |n|
-    result << n unless s.member?(n)
+  
+  missing = []
+  nums.count.times do |i|
+    missing << i+1 if nums[i] > 0
   end
-  result
+  
+  missing
 end
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+#def find_disappeared_numbers(nums)
+#  max = 0
+#  s = Set.new
+#  nums.each do |n|
+#    s.add(n)
+#  end
+#
+#  result = []
+#  1.upto(nums.count) do |n|
+#    result << n unless s.member?(n)
+#  end
+#  result
+#end
